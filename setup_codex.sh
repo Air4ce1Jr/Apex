615eb1f0844c62d6144c0a15dd442f9c8b461ca9
Air4ce1Jr <122828289+Air4ce1Jr@users.noreply.github.com>
2025-06-17 21:55:14 -0400

    Update setup_codex.sh

--- setup_codex.sh
+++ setup_codex.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 npm install -g sfdx-cli
 
+# install dos2unix if you don’t have it (macOS: brew install dos2unix)
+dos2unix setup_codex.sh
+
 # Non-interactive auth: reads from SFDX_AUTH_URL
 echo "$SFDX_AUTH_URL" | sfdx auth:sfdxurl:store --setalias QuickBooksSandbox
 
